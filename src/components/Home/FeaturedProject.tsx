a65f5ee3a74a9a5050e9f33d31da55cf0f24c523
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-04-10 22:43:01 +0700

    chore: adjusting project section

--- src/components/Home/FeaturedProject.tsx
+++ src/components/Home/FeaturedProject.tsx
@@ -11,10 +11,10 @@ const FeaturedProject = () => {
       <SectionTitle title="Featured project" />
 
       <div tw="relative rounded overflow-hidden border border-transparent bg-gray-100 shadow-lg hover:shadow-xl transition">
-        <Image src="/static/tuantanah.png" alt="TuanTanah" height={310} width={638} />
+        <Image src="/static/tuantanah.png" alt="TuanTanah" height={310} width={638} quality={95} />
 
         <div tw="px-5 pt-3 pb-5 space-y-2">
-          <p tw="font-bold text-3xl">Lorem ipsum dolor, sit amet</p>
+          <p tw="font-bold text-2xl md:text-3xl">Lorem ipsum dolor, sit amet</p>
           <p tw="text-gray-500 text-sm tracking-wide">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. In minus exercitationem eveniet aliquam iusto
             consequuntur, ipsam illum est blanditiis nulla nesciunt, dolorem ducimus voluptatibus debitis, nostrum rerum
