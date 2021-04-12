e2574cb14925c08b1409a168a22963606c6fd652
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-04-12 14:24:13 +0700

    chore: move haeder icons to Icons component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,23 +7,9 @@ import Logo from './Logo'
 import Routes from '@/config/Routes'
 import NextLink from '../ui/NextLink'
 import { LayoutWrapper } from '@/utils/styles'
+import { Hamburger, XClose } from '../ui/Icons'
 
 const navTotal = 4
-const XIcon = (
-  <svg tw="w-5 text-gray-600" viewBox="0 0 24 24">
-    <path
-      fill="currentColor"
-      d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
-    />
-  </svg>
-)
-const hamburgerIcon = (
-  <svg tw="w-5 text-gray-600" viewBox="0 0 24 24">
-    <path fill="currentColor" d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z" />
-    <path fill="currentColor" d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z" />
-    <path fill="currentColor" d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z" />
-  </svg>
-)
 
 const Header = () => {
   const { asPath } = useRouter()
@@ -56,7 +42,7 @@ const Header = () => {
               title="Open menu"
               onClick={() => setIsMenuOpen(true)}
             >
-              {hamburgerIcon}
+              <Hamburger />
             </button>
             {isMenuOpen && <NavModal setIsMenuOpen={setIsMenuOpen} asPath={asPath} />}
           </div>
@@ -81,7 +67,7 @@ const NavModal = ({ setIsMenuOpen, asPath }: { setIsMenuOpen: (boolean) => void;
           title="Close menu"
           onClick={() => setIsMenuOpen(false)}
         >
-          {XIcon}
+          <XClose />
         </button>
       </div>
       <nav>
