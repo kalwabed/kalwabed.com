7a48dc34a7dc57bf957d610e74ee4fcbed533690
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-04-12 17:11:16 +0700

    fix: og image url

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -7,7 +7,7 @@ const twitterUsername = '@' + app.socials.Twitter.split('/')[3]
 export const ogImgExtract = (text: string) => {
   const newText = encodeURI(text)
 
-  return `https://og-image.wzulfikar.com/**${newText}**.png?theme=custom&md=1&fontSize=150px&customBackground=%23000&customForeground=%23fff&customRadial=dimgray&backgroundImage=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1617642171314-276bb7641536%3Fixid%3DMXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%253D%26ixlib%3Drb-1.2.1%26auto%3Dformat%26fit%3Dcrop%26w%3D1400%26q%3D80&images=`
+  return `https://og-image.wzulfikar.com/i/**${newText}**.png?theme=custom&md=1&fontSize=150px&customBackground=%23000&customForeground=%23fff&customRadial=dimgray&backgroundImage=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1617642171314-276bb7641536%3Fixid%3DMXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%253D%26ixlib%3Drb-1.2.1%26auto%3Dformat%26fit%3Dcrop%26w%3D1400%26q%3D80&images=`
 }
 
 const defaultSeoConfig: NextSeoProps = {
@@ -31,7 +31,7 @@ const defaultSeoConfig: NextSeoProps = {
     images: [
       {
         url:
-          'https://og-image.wzulfikar.com/**kalwabed.com**.png?theme=custom&md=1&fontSize=150px&customBackground=%23000&customForeground=%23fff&customRadial=dimgray&backgroundImage=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1617642171314-276bb7641536%3Fixid%3DMXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%253D%26ixlib%3Drb-1.2.1%26auto%3Dformat%26fit%3Dcrop%26w%3D1400%26q%3D80&images=',
+          'https://og-image.wzulfikar.com/i/**kalwabed.com**.png?theme=custom&md=1&fontSize=150px&customBackground=%23000&customForeground=%23fff&customRadial=dimgray&backgroundImage=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1617642171314-276bb7641536%3Fixid%3DMXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%253D%26ixlib%3Drb-1.2.1%26auto%3Dformat%26fit%3Dcrop%26w%3D1400%26q%3D80&images=',
         alt: 'kalwabed.com default og-image'
       }
     ],
