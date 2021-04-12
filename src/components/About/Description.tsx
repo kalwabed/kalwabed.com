13c7cf02af5aeec50adaf5934fdc79d7be21fe79
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-04-12 21:53:44 +0700

    feat: about me page

---
+++ src/components/About/Description.tsx
@@ -0,0 +1,19 @@
+import 'twin.macro'
+
+const Description = () => {
+  return (
+    <div tw="leading-relaxed tracking-wider space-y-4">
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt quibusdam corrupti odit magni! Sed cum
+        voluptas tempora rem tenetur vero quos culpa numquam, modi voluptatem, fuga laborum reiciendis fugiat sapiente!
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit aperiam illum provident harum quam
+        possimus! Nihil excepturi doloribus, accusantium recusandae quam eum mollitia fugit, natus minima incidunt atque
+        asperiores placeat?
+      </p>
+    </div>
+  )
+}
+
+export default Description
--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,7 +1,18 @@
+import 'twin.macro'
 import ContentWrapper from '@/components/ContentWrapper'
+import Description from './Description'
+import Links from './Links'
 
 const AboutPageRenderer = () => {
-  return <ContentWrapper>the</ContentWrapper>
+  return (
+    <ContentWrapper>
+      <div tw="flex flex-1 justify-center items-center mx-auto md:mt-5">
+        <h2 tw="text-4xl font-bold">About</h2>
+      </div>
+      <Description />
+      <Links />
+    </ContentWrapper>
+  )
 }
 
 export default AboutPageRenderer
