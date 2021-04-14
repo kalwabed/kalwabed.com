772a1b31e8b78899634b52736dc44b27adc60fec
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-04-14 10:07:30 +0700

    feat: add content component in project page

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -1,4 +1,5 @@
 import 'twin.macro'
+import Image from 'next/image'
 
 import { Jumbotron } from '@/components/Projects/module'
 import { Container } from '@/utils/styles'
@@ -10,7 +11,28 @@ const ProjectPage = () => {
       <SEO title="TuanTanah" />
       <Jumbotron />
       <section css={[Container]}>
-        <p>the</p>
+        <div tw="w-full md:w-3/4 space-y-3">
+          <h1 tw="text-4xl font-bold">Head 1</h1>
+          <p tw="leading-relaxed tracking-wider text-gray-500">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit distinctio quidem nihil reprehenderit
+            explicabo aliquam consectetur quis, dolores doloremque voluptates, culpa qui repudiandae molestiae rerum
+            saepe facere! Provident, veniam maiores?
+          </p>
+        </div>
+
+        <div tw="flex flex-col justify-center lg:flex-row items-center my-24 space-y-2">
+          <div tw="w-full lg:w-1/2 overflow-hidden">
+            <Image src="/static/aku.jpg" alt="aku" width={500} height={412} tw="rounded" />
+          </div>
+          <div tw="w-full lg:w-1/2">
+            <h1 tw="text-4xl font-bold">Head 1</h1>
+            <p tw="leading-relaxed tracking-wider text-gray-500">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit distinctio quidem nihil reprehenderit
+              explicabo aliquam consectetur quis, dolores doloremque voluptates, culpa qui repudiandae molestiae rerum
+              saepe facere! Provident, veniam maiores?
+            </p>
+          </div>
+        </div>
       </section>
     </>
   )
