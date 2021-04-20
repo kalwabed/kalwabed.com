f8af922dd2f9683dac12593e01b239aaf0a15177
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-04-20 09:45:22 +0700

    chore: add docs to next.config

--- next.config.js
+++ next.config.js
@@ -15,12 +15,22 @@ module.exports = {
       }
     ]
   },
-  webpack: (config, { isServer }) => {
+  webpack: (config, { isServer, dev }) => {
+    // twin.macro webpack 4
     if (!isServer) {
       // Unset client-side javascript that only works server-side
       // https://github.com/vercel/next.js/issues/7755#issuecomment-508633125
       config.node = { fs: 'empty', module: 'empty' }
     }
+
+    // Replace React with Preact only in client production build
+    // if (!dev && !isServer) {
+    //   Object.assign(config.resolve.alias, {
+    //     react: 'preact/compat',
+    //     'react-dom/test-utils': 'preact/test-utils',
+    //     'react-dom': 'preact/compat'
+    //   })
+    // }
 
     return config
   }
