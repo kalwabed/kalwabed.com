f2357517cd160e8bd32436bb10e8918975327847
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-05-04 09:01:50 +0700

    chore: change description on home and about

--- src/components/About/Description.tsx
+++ src/components/About/Description.tsx
@@ -4,8 +4,8 @@ const Description = () => {
   return (
     <div tw="leading-relaxed tracking-wider space-y-3">
       <p>
-        My name is <b>Kalwabed Rizki</b>, I'm a student as well as a software engineer based in Indonesia. I'm currently
-        exploring modern website development using Javascript/Typescript.
+        My name is <b>Kalwabed Rizki</b>. A dreamer, and coffee connoisseur. Loves things related to modern web
+        development, Linux, and cybersecurity. Currently working as a frontend engineer at GoStartup.
       </p>
     </div>
   )
--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -10,9 +10,9 @@ const Hero = () => {
       </div>
       <div tw="space-y-5 lg:pr-12">
         <p tw="tracking-wide text-lg">Self-taught web developer based in Banyuwangi, Indonesia</p>
-        <p tw="text-gray-500 tracking-wide">
-          A dreamer, and coffee connoisseur. Loves things related to modern web development, Linux, and cybersecurity.
-          Currently working as a frontend engineer at GoStartup.
+        <p tw="text-gray-600 tracking-wide">
+          I'm a student as well as a software engineer based in Indonesia. I'm currently exploring modern website
+          development using Javascript/Typescript.
         </p>
         <div>
           <NextLink href="/about" title="Learn more about me" withIcon>
