a3ae010119dfc629d9dcc24e0241c59b4a4e99b0
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-05-07 20:05:48 +0700

    feat: add quick email at about page

--- src/components/About/Description.tsx
+++ src/components/About/Description.tsx
@@ -1,11 +1,18 @@
 import 'twin.macro'
 
+import app from '@/config/app'
+import NextLink from '../ui/NextLink'
+
 const Description = () => {
   return (
     <div tw="leading-relaxed tracking-wider space-y-3">
       <p>
         My name is <b>Kalwabed Rizki</b>. A dreamer, and coffee connoisseur. Loves things related to modern web
         development, Linux, and cybersecurity. Currently working as a frontend engineer at GoStartup.
+      </p>
+      <p>
+        Feel free to reach out if you're looking for a developer, have a question, or just want to connect.{' '}
+        <NextLink href={`mailto:${app.email}`}>kalwabedrzk@pm.me</NextLink>
       </p>
     </div>
   )
