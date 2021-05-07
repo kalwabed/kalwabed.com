b06396c7a02cdbeed3ebe02f8b1e4922681faed1
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-05-07 09:37:03 +0700

    feat: add my picture in about

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,13 +1,30 @@
-import 'twin.macro'
+import tw from 'twin.macro'
+import Image from 'next/image'
+
 import ContentWrapper from '@/components/ContentWrapper'
 import Description from './Description'
 import Links from './Links'
 import PageSectionTitle from '../PageSectionTitle'
 
+const ImgWrapper = tw.div`
+overflow-hidden
+w-auto
+flex
+justify-center
+items-center
+mx-auto
+shadow-2xl
+bg-slate
+rounded-xl
+`
+
 const AboutPageRenderer = () => {
   return (
     <ContentWrapper>
       <PageSectionTitle text="About" />
+      <ImgWrapper>
+        <Image src="/static/me.jpg" width={100} height={120} tw="rounded-xl" alt="My picture" />
+      </ImgWrapper>
       <Description />
       <Links />
     </ContentWrapper>
