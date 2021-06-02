d50e6669d8f78feb701c12f18ae9a78b8e19be5c
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-06-02 20:11:55 +0700

    chore: new og image

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -6,14 +6,17 @@ import app from '@/config/app'
 
 const twitterUsername = '@' + app.socials.Twitter.split('/')[3]
 const defaultOgImg =
-  'https://og-image.wzulfikar.com/i/.png?theme=custom&md=1&fontSize=100px&customBackground=%23000000&customForeground=%23ffffff&customRadial=dimgray&backgroundImage=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1576676830693-65730a608573%3Fixid%3DMnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8%26ixlib%3Drb-1.2.1%26auto%3Dformat%26fit%3Dcrop%26w%3D750%26q%3D80&images=https%3A%2F%2Fraw.githubusercontent.com%2Fkalwabed%2Fkalwabed.xyz%2Fmain%2Fpublic%2Fstatic%2Flogo-white.svg'
+  'https://i.microlink.io/https%3A%2F%2Fcards.microlink.io%2F%3Fpreset%3Djxnblk%26subtitle%3DWeb%2Bdeveloper%2Bbased%2Bin%2BIndonesia%26title%3Dkalwabed%26logo%3Dhttps%253A%252F%252Fkalwabed.xyz%252Fstatic%252F512.png%26p%3D2gKIPD4KICA8RmxleAogICAgc3g9e3sKICAgICAgYWxpZ25JdGVtczogJ2NlbnRlcicsCiAgICAgIGp1c3RpZnlDb250ZW50OiAnY2VudGVyJywKICAgICAgZmxleERpcmVjdGlvbjogJ2NvbHVtbicsCiAgICAgIGJnOiBxdWVyeS5iZywKICAgICAgZm9udEZhbWlseTonSW50ZXInLAogICAgICBweDogNSwKICAgIH19CiAgPgogICAgPEltYWdlIHN4PXt7IHdpZHRoOiAnMTI4cHgnIH19IHNyYz17cXVlcnkubG9nb30gLz4KICAgIDxUZXh0CiAgICAgIHN4PXt7CiAgICAgICAgcHk6IDQsCiAgICAgICAgdGV4dFRyYW5zZm9ybTogJ3VwcGVyY2FzZScsCiAgICAgICAgbGV0dGVyU3BhY2luZzogJzAuMmVtJywKICAgICAgICBsaW5lSGVpZ2h0OiAxLjI1LAogICAgICAgIGZvbnRXZWlnaHQ6IDgwMCwKICAgICAgICBmb250U2l6ZTogNCwKICAgICAgICBjb2xvcjogcXVlcnkuY29sb3IsCiAgICAgIH19CiAgICA-CiAgICAgIHtxdWVyeS50aXRsZX0KICAgIDwvVGV4dD4KICAgIDxUZXh0CiAgICAgIHN4PXt7CiAgICAgICAgZm9udFdlaWdodDogNTAwLAogICAgICAgIGZvbnRTaXplOiAzLAogICAgICAgIGNvbG9yOiBxdWVyeS5jb2xvciwKICAgICAgfX0KICAgID4KICAgICAge3F1ZXJ5LnN1YnRpdGxlfQogICAgPC9UZXh0PgogIDwvRmxleD4KPC8-'
 
 const defaultSeoConfig: NextSeoProps = {
   defaultTitle: 'Home',
   titleTemplate: '%s | Kalwabed',
   description: app.description,
   canonical: app.siteUrl,
-  additionalMetaTags: [{ name: 'keywords', content: 'kalwabed rizki, kalwabed, rizki, portfolio, blog, developer' }],
+  additionalMetaTags: [
+    { name: 'keywords', content: 'kalwabed rizki, kalwabed, rizki, portfolio, blog, developer' },
+    { name: 'image', content: defaultOgImg }
+  ],
   additionalLinkTags: [
     { rel: 'icon', href: '/favicon.ico' },
     { rel: 'icon', href: '/static/icon.svg', type: 'image/svg+xml' },
@@ -29,9 +32,9 @@ const defaultSeoConfig: NextSeoProps = {
     type: 'website',
     images: [
       {
-        url: defaultOgImg,
         width: 1200,
         height: 628,
+        url: defaultOgImg,
         alt: 'kalwabed.xyz default og-image'
       }
     ],
