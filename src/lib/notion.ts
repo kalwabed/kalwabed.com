7673a9483f2686042b05d976227db662eba5f200
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-07-02 06:30:07 +0700

    fix: type error

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -1,9 +1,9 @@
-import { Bookmarks } from '@/types'
+import { Bookmark } from '@/types'
 import { Client } from '@notionhq/client'
 
-export const getDatabases = async (): Promise<Bookmarks[]> => {
+export const getDatabases = async (): Promise<Bookmark[]> => {
   const notion = new Client({ auth: process.env.NOTION_SECRET })
   const bookmarks = await notion.databases.query({ database_id: process.env.NOTION_BOOKMARKS_ID })
 
-  return bookmarks.results.map(res => res.properties) as unknown as Bookmarks[]
+  return bookmarks.results.map(res => res.properties) as unknown as Bookmark[]
 }
