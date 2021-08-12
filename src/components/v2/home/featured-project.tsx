a1e831c7bb1c380771ab10b7f83089b01538487b
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-08-12 20:57:21 +0700

    chore: responsive featured project

--- src/components/v2/home/featured-project.tsx
+++ src/components/v2/home/featured-project.tsx
@@ -10,6 +10,7 @@ import NextLink from '../shared/NextLink'
 const _Wrapper = tw.div`
 w-full
 flex justify-between
+flex-col lg:flex-row
 bg-subtleAppBg
 border border-appBorder
 overflow-hidden
@@ -24,17 +25,17 @@ const FeaturedProject = ({ project }) => {
 
       <_Wrapper>
         <Image src={FeaturedBanner} alt="Featured project banner" placeholder="blur" />
-        <section tw="w-full p-8 flex flex-col">
+        <section tw="w-full p-6 lg:p-8 flex flex-col">
           <Link href="/projects/tuantanah" passHref>
             <a tw="text-4xl font-bold transition hocus:text-link">
               <h2>TuanTanah</h2>
             </a>
           </Link>
-          <small tw="text-sm text-lowContrast mt-3.5">React, Next.js, MongoDB, Express.js</small>
-          <p tw="text-lowContrast mt-11">
+          <small tw="text-sm text-lowContrast mt-2 lg:mt-3.5">React, Next.js, MongoDB, Express.js</small>
+          <p tw="text-lowContrast mt-6 lg:mt-11">
             A web application that is intended as a meeting place between land sellers and potential buyers.
           </p>
-          <div tw="mt-auto">
+          <div tw="mt-6 lg:mt-auto">
             <NextLink href="/project/tuantanah">More detail</NextLink>
           </div>
         </section>
