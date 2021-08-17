cfeb7abd6a162ba6410ffc47c6eb992207f096fe
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-08-17 08:59:33 +0700

    feat: projects

--- src/components/v2/home/featured-project.tsx
+++ src/components/v2/home/featured-project.tsx
@@ -4,9 +4,10 @@ import tw from 'twin.macro'
 
 import { Container } from '../shared/container'
 import { MoreLink, SectionTitle } from './modules'
-import FeaturedBanner from '@components/v2/home/assets/featured-project-banner.png'
 import TextLink from '../shared/text-link'
 import { Project } from '@/types'
+import TechList from '../shared/tech-list'
+import projectBanner from '../shared/project-banners'
 
 const _Wrapper = tw.div`
 w-full
@@ -20,24 +21,26 @@ shadow
 `
 
 const FeaturedProject: React.FC<{ project: Project }> = ({ project }) => {
+  const { shortDescription, slug, techs, title } = project
+
   return (
     <section css={[Container]}>
       <SectionTitle title="Featured project" />
 
       <_Wrapper>
-        <Image src={FeaturedBanner} alt="Featured project banner" placeholder="blur" />
+        <Image src={projectBanner[slug]} alt="Featured project banner" placeholder="blur" />
         <section tw="w-full p-6 lg:p-8 flex flex-col">
-          <Link href="/projects/tuantanah" passHref>
+          <Link href={`/projects/${slug}`} passHref>
             <a tw="text-4xl font-bold transition hocus:text-link">
-              <h2>TuanTanah</h2>
+              <h2>{title}</h2>
             </a>
           </Link>
-          <small tw="text-sm text-lowContrast mt-2 lg:mt-3.5">React, Next.js, MongoDB, Express.js</small>
-          <p tw="text-lowContrast mt-6 lg:mt-11">
-            A web application that is intended as a meeting place between land sellers and potential buyers.
-          </p>
+          <small tw="text-sm text-lowContrast mt-2 lg:mt-3.5">
+            <TechList techs={techs} />
+          </small>
+          <p tw="text-lowContrast mt-6 lg:mt-11">{shortDescription}</p>
           <div tw="mt-6 lg:mt-auto">
-            <TextLink href="/projects/tuantanah">More detail</TextLink>
+            <TextLink href={`/projects/${slug}`}>More detail</TextLink>
           </div>
         </section>
       </_Wrapper>
--- src/components/v2/projects/modules/project-card.tsx
+++ src/components/v2/projects/modules/project-card.tsx
@@ -3,30 +3,31 @@ import tw from 'twin.macro'
 
 import ButtonLink from '@components/v2/shared/button-link'
 import TextLink from '@components/v2/shared/text-link'
-import BannerExample from '../assets/banner.png'
 import { Project } from '@/types'
+import projectBanner from '@components/v2/shared/project-banners'
+import TechList from '@components/v2/shared/tech-list'
 
 const ProjectCard = ({ project }: { project: Project }) => {
-  const { banner, description, title, liveUrl, githubRepo, slug, techs, publishedAt } = project
+  const { description, title, liveUrl, githubRepo, slug, techs, publishedAt } = project
 
   return (
     <section tw="flex flex-col space-y-8">
       <div tw="flex flex-col space-y-1">
         <div>
-          <TextLink variant="ghost" href={`/project/${slug}`} styles={tw`text-4xl`}>
+          <TextLink variant="ghost" href={`/projects/${slug}`} styles={tw`text-4xl`}>
             {title}
           </TextLink>
         </div>
         <div tw="inline-flex items-center space-x-2.5">
           <span tw="w-12 h-1 bg-lowContrast" />{' '}
           <span tw="text-lowContrast tracking-wide">
-            {new Date(publishedAt).getFullYear()} - {techs.map((tech, index) => (index ? ', ' : '') + tech)}
+            {new Date(publishedAt).getFullYear()} - <TechList techs={techs} />
           </span>
         </div>
       </div>
 
       <div tw="overflow-hidden mx-5 rounded-lg">
-        <Image src={banner} width={1000} height={500} tw="rounded-lg" alt={`${title} preview banner`} />
+        <Image src={projectBanner[slug]} placeholder="blur" tw="rounded-lg" alt={`${title} preview banner`} />
       </div>
 
       <div tw="flex flex-col space-y-4">
