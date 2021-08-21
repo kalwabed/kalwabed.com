325ee752a5770d3dc3f6a482c18b9446d6dff0e6
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2021-08-21 10:58:04 +0700

    update projects SEO descriptio

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -15,7 +15,7 @@ export const getStaticProps: GetStaticProps = async () => {
 const ProjectsPage: NextPage<{ projects: Project[] }> = ({ projects }) => {
   return (
     <Page>
-      <SEO title="Projects" description="Where my projects life." />
+      <SEO title="Projects" description="Where I show off my projects." />
       <ProjectsPageRenderer projects={projects} />
     </Page>
   )
