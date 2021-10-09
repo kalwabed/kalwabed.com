9d60c7a344ff916d6e15050b3bab294361b0722e
kalwabed <49640654+kalwabed@users.noreply.github.com>
2021-10-09 14:04:52 +0700

    sign

--- src/components/global-styles.tsx
+++ src/components/global-styles.tsx
@@ -1,5 +1,5 @@
-import tw, { theme, GlobalStyles as BaseStyles } from 'twin.macro'
 import { Global, css } from '@emotion/react'
+import tw, { theme, GlobalStyles as BaseStyles } from 'twin.macro'
 
 const CustomStyle = css`
   * {
