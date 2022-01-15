c41b7e8d786164366fbb23ec84e4c887fb19507d
kalwabed <49640654+kalwabed@users.noreply.github.com>
2022-01-15 07:50:05 +0700

    fix: process not defined

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '~styles/tailwind.css'
 import '~styles/global.css'
 import '~styles/mdx.css'
+import Script from 'next/script'
 
 import { DefaultSEO } from '~components/SEO'
 import LayoutRoot from '~components/layout/layout-root'
@@ -8,10 +9,15 @@ import Page from '~components/layout/page'
 import { AppPropsWithLayout } from '~types'
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  if (typeof window === 'undefined') return null
+
+  const umamiUrl = process.env.NEXT_PUBLIC_UMAMI_URL
+  const umamiId = process.env.NEXT_PUBLIC_UMAMI_ID
   const getLayout = Component.getLayout ?? (page => page)
 
   return (
     <>
+      <Script strategy="afterInteractive" src={umamiUrl} data-website-id={umamiId} />
       <DefaultSEO />
       {getLayout(
         <LayoutRoot>
