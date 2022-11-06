c3f00c24e12e3bc24ff0cc53673cabe2def90b68
kalwabed <49640654+kalwabed@users.noreply.github.com>
2022-11-06 18:41:37 +0700

    fix: page navigation

--- src/components/layout/top-navigation.tsx
+++ src/components/layout/top-navigation.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import Link from 'next/link'
 import { useState } from 'react'
 import { HiOutlineMenu } from 'react-icons/hi'
--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -4,7 +4,9 @@ import { memo } from 'react'
 import WhiteLogo from '~assets/logo-white.png'
 
 const Logo = () => {
-  return <Image src={WhiteLogo} sizes="10vw" placeholder="blur" alt="kalwabed.xyz logo" />
+  return (
+    <Image src={WhiteLogo} sizes="10vw" placeholder="blur" alt="kalwabed.xyz logo" className="umami--click--logo" />
+  )
 }
 
 export default memo(Logo)
