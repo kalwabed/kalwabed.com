dcb82f0526085f0f9a2a3029db77f1650f5c8c2f
kalwabed <49640654+kalwabed@users.noreply.github.com>
2023-10-09 21:55:53 +0700

    chore: update deps

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@
  */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    legacyBrowsers: false,
-    appDir: false
-  },
   images: {
     formats: ['image/avif', 'image/webp']
   },
