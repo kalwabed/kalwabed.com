bf4cb23ce8ecb53a6071ef5645f7714bb111c726
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-01-19 07:09:45 +0700

    fix: tosorted

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -2,7 +2,7 @@ import { getCollection } from "astro:content";
 
 export async function getSortedPosts() {
 	const allBlogPosts = await getCollection("blog");
-	const sortedBlog = allBlogPosts.toSorted((a, b) => {
+	const sortedBlog = allBlogPosts.sort((a, b) => {
 		return (
 			new Date(b.data.publishDate).getTime() -
 			new Date(a.data.publishDate).getTime()
