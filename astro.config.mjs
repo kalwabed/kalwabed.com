360da0f0b93cf3531267c00fe8676f516bf65fc9
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-01-20 02:14:56 +0700

    feat: integrating first mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,24 @@
+import a11yEmoji from "@fec/remark-a11y-emoji";
 import { defineConfig, squooshImageService } from "astro/config";
-import a11yEmoji from '@fec/remark-a11y-emoji'
 import Icons from "unplugin-icons/vite";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	image: {
-		service: squooshImageService(),
-	},
-	markdown: {
-		remarkPlugins: [a11yEmoji],
-		shikiConfig: {
-			theme: 'vitesse-light'
-		}
-	},
-	vite: {
-		plugins: [
-			Icons({
-				compiler: "astro",
-				autoInstall: true,
-			}),
-		],
-	},
+  image: {
+    service: squooshImageService()
+  },
+  markdown: {
+    remarkPlugins: [a11yEmoji],
+    shikiConfig: {
+      theme: "vitesse-light"
+    }
+  },
+  vite: {
+    plugins: [Icons({
+      compiler: "astro",
+      autoInstall: true
+    })]
+  },
+  integrations: [mdx()]
 });
