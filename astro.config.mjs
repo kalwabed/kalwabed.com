03bab563d6f03493c42d00370abe4c3720e3b884
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-01-20 01:12:03 +0700

    feat: add a11y emoji plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig, squooshImageService } from "astro/config";
+import a11yEmoji from '@fec/remark-a11y-emoji'
 import Icons from "unplugin-icons/vite";
 
 // https://astro.build/config
 export default defineConfig({
 	image: {
 		service: squooshImageService(),
+	},
+	markdown: {
+		remarkPlugins: [a11yEmoji],
+		shikiConfig: {
+			theme: 'vitesse-light'
+		}
 	},
 	vite: {
 		plugins: [
