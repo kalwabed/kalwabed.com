25364612bf365fd01a9de8296e26c9605367241d
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-01-20 02:57:59 +0700

    ref: migrating existing blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,26 @@
+import mdx from "@astrojs/mdx";
 import a11yEmoji from "@fec/remark-a11y-emoji";
 import { defineConfig, squooshImageService } from "astro/config";
 import Icons from "unplugin-icons/vite";
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    service: squooshImageService()
-  },
-  markdown: {
-    remarkPlugins: [a11yEmoji],
-    shikiConfig: {
-      theme: "vitesse-light"
-    }
-  },
-  vite: {
-    plugins: [Icons({
-      compiler: "astro",
-      autoInstall: true
-    })]
-  },
-  integrations: [mdx()]
+	image: {
+		service: squooshImageService(),
+	},
+	markdown: {
+		remarkPlugins: [a11yEmoji],
+		shikiConfig: {
+			theme: "vitesse-light",
+		},
+	},
+	vite: {
+		plugins: [
+			Icons({
+				compiler: "astro",
+				autoInstall: true,
+			}),
+		],
+	},
+	integrations: [mdx()],
 });
