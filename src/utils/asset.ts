f60c10db84b3f5c640982b996d6aa776419122a5
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-01-25 16:39:39 +0700

    ref: using dynamic img instead

--- src/utils/asset.ts
+++ src/utils/asset.ts
@@ -1,19 +1,19 @@
-export function getProjectImg(imgPath: string) {
+export function getDynamicImage(imgPath: string) {
 	if (!imgPath) {
 		throw new Error("imgPath is not found!");
 	}
 
-	const projectAssetsFolder = "/src/assets/projects";
+	const localAssetsFolder = "/src/assets";
 	const images = import.meta.glob<{ default: ImageMetadata }>(
-		"/src/assets/projects/**/*.{jpeg,jpg,png,gif}",
+		"/src/assets/**/*.{jpeg,jpg,png,gif}",
 	);
-	const projectCover = images[`${projectAssetsFolder}${imgPath}`];
+	const image = images[`${localAssetsFolder}${imgPath}`];
 
-	if (!projectCover) {
+	if (!image) {
 		throw new Error(
-			`"${imgPath} does not exist in glob: "${projectAssetsFolder}/*.{jpeg,jpg,png,gif}"`,
+			`"${imgPath} does not exist in glob: "${localAssetsFolder}/*.{jpeg,jpg,png,gif}"`,
 		);
 	}
 
-	return projectCover;
+	return image;
 }
