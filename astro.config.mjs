b7fcb736b99d9d7982290a371f8e6d5d803a2193
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-01-25 14:08:47 +0700

    feat: add autoprefixer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,34 @@
 import mdx from "@astrojs/mdx";
+import autoprefixer from "autoprefixer";
 import a11yEmoji from "@fec/remark-a11y-emoji";
 import { defineConfig, squooshImageService } from "astro/config";
 import Icons from "unplugin-icons/vite";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
 
-import sitemap from "@astrojs/sitemap";
-
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.kalwabed.xyz",
-  image: {
-    service: squooshImageService()
-  },
-  markdown: {
-    remarkPlugins: [a11yEmoji, remarkReadingTime],
-    shikiConfig: {
-      theme: "vitesse-light"
-    }
-  },
-  vite: {
-    plugins: [Icons({
-      compiler: "astro",
-      autoInstall: true
-    })]
-  },
-  integrations: [mdx(), sitemap()]
+	site: "https://www.kalwabed.xyz",
+	image: {
+		service: squooshImageService(),
+	},
+	markdown: {
+		remarkPlugins: [a11yEmoji, remarkReadingTime],
+		shikiConfig: {
+			theme: "vitesse-light",
+		},
+	},
+	vite: {
+		css: {
+			postcss: {
+				plugins: [autoprefixer],
+			},
+		},
+		plugins: [
+			Icons({
+				compiler: "astro",
+				autoInstall: true,
+			}),
+		],
+	},
+	integrations: [mdx()],
 });
