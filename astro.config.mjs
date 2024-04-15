df6f5e9f7faab7abead7ebe521770b79c10378b6
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-04-15 07:05:47 +0700

    feat: initial bookmarks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import a11yEmoji from "@fec/remark-a11y-emoji";
+import cloudflare from "@astrojs/cloudflare";
 import { defineConfig, squooshImageService } from "astro/config";
 import autoprefixer from "autoprefixer";
 import Icons from "unplugin-icons/vite";
@@ -8,28 +9,28 @@ import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.kalwabed.com",
-	image: {
-		service: squooshImageService(),
-	},
-	markdown: {
-		remarkPlugins: [a11yEmoji, remarkReadingTime],
-		shikiConfig: {
-			theme: "vitesse-light",
-		},
-	},
-	vite: {
-		css: {
-			postcss: {
-				plugins: [autoprefixer],
-			},
-		},
-		plugins: [
-			Icons({
-				compiler: "astro",
-				autoInstall: true,
-			}),
-		],
-	},
-	integrations: [mdx(), sitemap()],
+  site: "https://www.kalwabed.com",
+  output: 'hybrid',
+  image: {
+    service: squooshImageService()
+  },
+  markdown: {
+    remarkPlugins: [a11yEmoji, remarkReadingTime],
+    shikiConfig: {
+      theme: "vitesse-light"
+    }
+  },
+  vite: {
+    css: {
+      postcss: {
+        plugins: [autoprefixer]
+      }
+    },
+    plugins: [Icons({
+      compiler: "astro",
+      autoInstall: true
+    })]
+  },
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare(), 
 });
