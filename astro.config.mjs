ec99867e61075c2505ab84b7372ec830895795a2
Kalwabed Rizki <49640654+kalwabed@users.noreply.github.com>
2025-04-27 18:32:27 +0700

    perf: use astro fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import a11yEmoji from "@fec/remark-a11y-emoji";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import autoprefixer from "autoprefixer";
 import Icons from "unplugin-icons/vite";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
@@ -33,4 +33,30 @@ export default defineConfig({
   adapter: cloudflare({
     imageService: "compile",
   }),
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.bunny(),
+        name: "Overpass",
+        cssVariable: "--font-body",
+        fallbacks: ["Open Sans", "sans-serif"],
+        weights: [400, 500, 600, 700],
+        subsets: ["latin"],
+      },
+      {
+        provider: fontProviders.bunny(),
+        name: "Schoolbell",
+        fallbacks: ["Cochin", "serif"],
+        cssVariable: "--font-heading",
+        weights: [400, 500, 600, 700],
+        subsets: ["latin"],
+      },
+      {
+        provider: fontProviders.bunny(),
+        name: "Fira Code",
+        fallbacks: ["Courier", "monospace"],
+        cssVariable: "--font-mono",
+      },
+    ],
+  },
 });
